9bcf5d1ef4c8cff957426a1f4083822acb6fbe55
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2021-02-23 00:00:00 -0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 let time = 0
+input.onButtonPressed(Button.A, function () {
+    while (!(input.buttonIsPressed(Button.B))) {
+        basic.pause(1000)
+        time += 1
+    }
+})
 input.onButtonPressed(Button.AB, function () {
     time = 0
     basic.showNumber(time)
